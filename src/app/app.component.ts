0cdc94a6ce1f0ce59d66860d7524b79fc3abb330
dherediat <dherediat97@gmail.com>
2024-10-12 23:16:30 +0200

    feature: add text detection(barcode)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component } from '@angular/core'
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-root',
---
+++ src/app/home/home.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoddieAppService {
+  getFoodData(barcode: String) {
+    try {
+    } catch (error) {}
+  }
+}
